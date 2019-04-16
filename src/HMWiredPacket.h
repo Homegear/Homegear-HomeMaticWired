6ac16b752a2efc98b57399d081c5b0720e5d292d
Dr. Sathya Laufer <s.laufer@homegear.email>
2019-04-16 16:05:24 +0200

    Fixed: Symbol not found error

--- src/HMWiredPacket.h
+++ src/HMWiredPacket.h
@@ -84,7 +84,7 @@ public:
     std::vector<uint8_t> getPosition(double index, double size, int32_t mask);
     void setPosition(double index, double size, std::vector<uint8_t>& value);
 private:
-    static constexpr std::array<uint8_t, 9> _bitmask{0xFF, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+    static const std::array<uint8_t, 9> _bitmask;
 
     //Packet content
     uint8_t _length = 0;
