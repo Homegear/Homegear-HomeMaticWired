f9a215b9cebd28ae1601b897b969fcdf7fb42ae0
Dr. Sathya Laufer <s.laufer@homegear.email>
2019-04-23 21:01:50 +0200

    Base library changes

--- src/HMWiredPeer.cpp
+++ src/HMWiredPeer.cpp
@@ -1480,6 +1480,9 @@ bool HMWiredPeer::load(BaseLib::Systems::ICentral* central)
 			GD::out.printError("Error loading HomeMatic Wired peer " + std::to_string(_peerID) + ": Device type not found: 0x" + BaseLib::HelperFunctions::getHexString((uint32_t)_deviceType) + " Firmware version: " + std::to_string(_firmwareVersion));
 			return false;
 		}
+
+		if(_rpcDevice->memorySize == -1) _rpcDevice->memorySize = 1024;
+
 		initializeTypeString();
 		std::string entry;
 		loadConfig();
